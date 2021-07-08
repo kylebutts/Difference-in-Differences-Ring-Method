fcf03b676eb94f4ce84c3085ecb49a581b90090f
Kyle F. Butts <buttskyle96@gmail.com>
2021-07-08 15:15:09 -0600

    Update figures
    Removed parallel trends violation
    Improved readability in paper

--- figure-example_problems.R
+++ figure-example_problems.R
@@ -1,171 +1,293 @@
-## -----------------------------------------------------------------------------
-## figure-example_problems.R
+## figure-example_problems.R ---------------------------------------------------
 ## Kyle Butts, CU Boulder Economics
 ##
 ## Problem with ad-hoc approach to selecting ring
-## -----------------------------------------------------------------------------
 
 library(tidyverse)
 library(glue)
+library(sf)
+library(patchwork)
 
 # Load Custom Theme
 source("https://raw.githubusercontent.com/kylebutts/templates/master/ggplot_theme/theme_kyle.R")
 
 # ggpreview
 ggpreview <- function(..., device = "png", cairo = TRUE) {
-	fname <- tempfile(fileext = paste0(".", device))
+    fname <- tempfile(fileext = paste0(".", device))
 
-	if (cairo & device == "pdf") {
-		ggplot2::ggsave(filename = fname, device = cairo_pdf, ...)
-	} else if (cairo & device == "png") {
-		ggplot2::ggsave(filename = fname, device = device, type = "cairo", ...)
-	} else {
-		ggplot2::ggsave(filename = fname, device = device, ...)
-	}
+    if (cairo & device == "pdf") {
+        ggplot2::ggsave(filename = fname, device = cairo_pdf, ...)
+    } else if (cairo & device == "png") {
+        ggplot2::ggsave(filename = fname, device = device, type = "cairo", ...)
+    } else {
+        ggplot2::ggsave(filename = fname, device = device, ...)
+    }
 
-	system2("open", fname)
-	invisible(NULL)
+    system2("open", fname)
+    invisible(NULL)
 }
+
+
+
+# ---- Example of ID strategy --------------------------------------------------
+
+set.seed(2021)
+
+# Full rectangle
+rectangle <- st_sf(
+    id = 1,
+    geometry = st_sfc(
+        st_polygon(list(rbind(c(0, 0), c(2, 0), c(2, 2), c(0, 2), c(0, 0))))
+    )
+)
+
+# Treatment point
+treat <- st_sf(
+    id = 1,
+    geometry = st_sfc(
+        st_point(c(1, 1))
+    )
+)
+
+# Rings
+treat_ring <- st_buffer(treat, dist = 0.25)
+control_ring <- st_difference(st_buffer(treat, dist = 0.5), treat_ring)
+
+# Random sample of points
+points <- st_sf(id = 1:2000, st_sample(rectangle, 2000)) %>%
+    mutate(
+        group = case_when(
+            st_within(., treat_ring, sparse = F) ~ "Treated",
+            st_within(., control_ring, sparse = F) ~ "Control",
+            TRUE ~ NA_character_
+        )
+    )
+
+# Plot figure
+(plot_id <- ggplot() +
+    geom_sf(data = rectangle, fill = NA) +
+    geom_sf(data = treat_ring, fill = NA, alpha = 0.2) +
+    geom_sf(data = control_ring, fill = NA, alpha = 0.2) +
+    geom_sf(data = points, mapping = aes(color = group)) +
+    geom_sf(data = treat, color = "black", shape = 3, size = 2) +
+    coord_sf(datum = NULL) +
+    scale_color_manual(
+        values = c("Treated" = "#6A6599", "Control" = "#79AF97"),
+        na.value = "grey50"
+    ) +
+    labs(color = "Treatment Status") +
+    theme_kyle(base_size = 12) +
+    theme(
+        legend.position = c(0.25, 0.175),
+        legend.background = element_rect(fill = "white")
+    ))
+
+ggsave("figures/example_id.pdf", plot_id, dpi = 300, width = 5, height = 5, bg = "white")
+
 
 
 # ---- Generate Data -----------------------------------------------------------
 
-df1 <- tibble(id = 1:500) %>%
-	# Generate Treatment and Distance
-	mutate(
-		dist = runif(n(), 0, 2),
-		dist = dist
-	) %>%
-	# Treatment Effects
-	mutate(
-		spill1 = 1.5 * exp(-2 * dist) * (dist <= 1),
-		eps = rnorm(n(), mean = 0, sd = 0.05),
-		`Treatment Effect` = spill1,
-		`Counterfactual Trend` = 0, # dist * 0.075,
-		`Observed Change` = `Treatment Effect` + `Counterfactual Trend`
-	)
+set.seed(20210708)
+
+df <- tibble(id = 1:10000) %>%
+    # Generate Treatment and Distance
+    mutate(
+        dist = runif(n(), 0, 1.5),
+        dist = dist
+    ) %>%
+    # Treatment Effects
+    mutate(
+        spill1 = 1.5 * exp(-2.3 * dist) * (dist <= 0.75),
+        eps = rnorm(n(), mean = 0, sd = 0.05),
+        `Treatment Effect` = spill1,
+        `Counterfactual Trend` = 0, #dist * 0.075,
+        `Observed Change` = `Treatment Effect` + `Counterfactual Trend`
+    )
 
 
-df1_long = df1 %>% pivot_longer(
-		cols = c(`Treatment Effect`, `Counterfactual Trend`)
-	)
+df_long <- df %>% pivot_longer(
+    cols = c(`Treatment Effect`, `Counterfactual Trend`, `Observed Change`)
+)
+
+true_te <- df %>% filter(dist <= 0.75) %>% pull(`Treatment Effect`) %>% mean()
+
 
 # Preview of Data
 ggplot() +
-	geom_point(data = df1_long, aes(x=dist, y=value, color=name), shape = 19, alpha = 0.8) +
-	scale_color_manual(
-		values = c("#374E55", "#B24745", "#00A1D5", "#DF8F44",  "#79AF97", "#6A6599")
-	) +
-	labs(
-		y = "Change in Outcome", shape = NULL, color = NULL, x = "Distance (miles)"
-	) +
-	theme_kyle(base_size = 16) +
-	theme(legend.position = "bottom")
+    geom_point(data = df_long, aes(x = dist, y = value, color = name), shape = 19, alpha = 0.8) +
+    scale_color_manual(
+        values = c("#374E55", "#B24745", "#00A1D5", "#DF8F44", "#79AF97", "#6A6599")
+    ) +
+    labs(
+        y = "Change in Outcome", shape = NULL, color = NULL, x = "Distance (miles)"
+    ) +
+    theme_kyle(base_size = 16) +
+    theme(legend.position = "bottom")
 
 
 # ---- Plot function -----------------------------------------------------------
 
-plot_est = function(df, dist_t, dist_c) {
-	df = df %>% mutate(
-		group = case_when(
-			dist <= dist_t ~ "Treated",
-			dist <= dist_c ~ "Control",
-			TRUE ~ NA_character_
-		)
-	) %>%
-		drop_na(group)
+plot_est <- function(df, df_long, dist_t, dist_c, title = NULL, include_labs = F) {
+    df <- df %>%
+        mutate(
+            group = case_when(
+                dist <= dist_t ~ "Treated",
+                dist <= dist_c ~ "Control",
+                TRUE ~ NA_character_
+            )
+        ) %>%
+        drop_na(group)
 
-	mean_t = mean(df[df$group == "Treated",]$`Observed Change`)
-	mean_c = mean(df[df$group == "Control",]$`Observed Change`)
+    mean_t <- mean(df[df$group == "Treated", ]$`Observed Change`)
+    mean_c <- mean(df[df$group == "Control", ]$`Observed Change`)
 
-	avg_pt = tribble(
-		~x, ~y, ~group,
-		0, mean_t, "Treat",
-		dist_t, mean_t, "Treat",
-		dist_t, mean_c, "Control",
-		dist_c, mean_c, "Control"
-	)
+    avg_pt <- tribble(
+        ~x, ~y, ~group,
+        0, mean_t, "Treat",
+        dist_t, mean_t, "Treat",
+        dist_t, mean_c, "Control",
+        dist_c, mean_c, "Control"
+    )
 
-	rect = tribble(
-		~xmin, ~xmax, ~ymin, ~ymax, ~group,
-		0, dist_t, -Inf, Inf, "Treat",
-		dist_t, dist_c, -Inf, Inf, "Control"
-	)
+    rect <- tribble(
+        ~xmin, ~xmax, ~ymin, ~ymax, ~group,
+        0, dist_t, -Inf, Inf, "Treated Ring",
+        dist_t, dist_c, -Inf, Inf, "Control Ring"
+    )
 
-	max_y = max(df$`Observed Change`)
-	labs = tribble(
-		~x, ~y, ~label,
-		dist_t/2, max_y, "\u2190 Treated Units \u2192",
-		dist_t + (dist_c - dist_t)/2, max_y, "\u2190 Control Units \u2192",
-	)
+    max_y <- max(df$`Observed Change`) * 0.98
+    labs <- tribble(
+        ~x, ~y, ~label,
+        # dist_t/2, max_y, "\u2190 Treated Units \u2192",
+        # dist_t + (dist_c - dist_t)/2, max_y, "\u2190 Control Units \u2192",
+        dist_t / 2, max_y, "Treated Units",
+        dist_t + (dist_c - dist_t) / 2, max_y, "Control Units",
+    )
 
-	ggplot() +
-		geom_rect(data = rect %>% filter(group == "Treat"),
-							mapping = aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
-							alpha = 0.2, fill = "#6A6599", color = NA
-		) +
-		geom_rect(data = rect %>% filter(group == "Control"),
-							mapping = aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
-							alpha = 0.2, fill = "#79AF97", color = NA
-		) +
-		geom_point(data = df1_long,
-							 mapping = aes(x=dist, y=value, color=name, shape=name)
-		) +
-		geom_line(data = avg_pt %>% filter(group == "Treat"), mapping = aes(x = x, y = y),
-							color = "#6A6599", size = 1.1
-		) +
-		geom_line(data = avg_pt %>% filter(group == "Control"), mapping = aes(x = x, y = y),
-							color = "#79AF97", size = 1.1
-		) +
-		# Treated and Control Unit Labels
-		geom_label(data = labs,
-							 mapping = aes(x = x, y = y, label = label), size = 5, fill = NA
-		) +
-		# Mean Labels
-		annotate("label", x = dist_t - 0.02, y = mean_t + 0.1, label = "bar(Delta*y)[T]",
-						 parse=T, hjust = 1, size = 5, color = "#6A6599", fill = NA) +
-		annotate("label", x = dist_c - 0.02, y = mean_c + 0.1, label = "bar(Delta*y)[C]",
-						parse=T, hjust = 1, size = 5, color = "#79AF97", fill = NA) +
-		# Difference-in-differences Labels
-		annotate("point", x = dist_t, y = c(mean_t, mean_c), size = 2) +
-		annotate("segment", x = dist_t, xend = dist_t, y = mean_t, yend = mean_c,
-						 linetype = "dashed", size = 1.1) +
-		annotate("text", x = dist_t + 0.03, y = mean_c + (mean_t - mean_c)*2/3,
-						 label = "hat(tau)",
-						 parse=T, hjust = 0, size = 7) +
-		scale_color_manual(
-			values = c("Counterfactual Trend" = "grey60", "Treatment Effect" = "grey20")
-		) +
-		scale_shape_manual(
-			values = c("Counterfactual Trend" = 4, "Treatment Effect" = 16)
-		) +
-		scale_x_continuous(expand = expansion(mult = c(0, 0)), limits = c(0, 2)) +
-		labs(
-			y = "Change in Outcome", x = "Distance from Treatment (miles)", shape = NULL, color = NULL
-		) +
-		theme_kyle(base_size = 16) +
-		theme(legend.position = "bottom")
+    # avoid the line in the drop
+    df_long <- bind_rows(
+        df_long %>% filter(dist <= 0.75),
+        tibble(dist = 0.75, name = "Treatment Effect", value = NA),
+        df_long %>% filter(dist > 0.75),
+    )
 
+    (plot <- ggplot() +
+        # Shaded regions
+        geom_rect(
+            data = rect,
+            mapping = aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = group),
+            alpha = 0.2, color = NA
+        ) +
+        # Potential outcomes
+        geom_line(
+            data = df_long %>% filter(name != "Observed Change"),
+            mapping = aes(x = dist, y = value, color = name),
+            size = 1.5
+        ) +
+        # Mean of treated and untreated
+        geom_line(
+            data = avg_pt %>% filter(group == "Treat"), mapping = aes(x = x, y = y),
+            color = "#6A6599", size = 1.1
+        ) +
+        geom_line(
+            data = avg_pt %>% filter(group == "Control"), mapping = aes(x = x, y = y),
+            color = "#79AF97", size = 1.1
+        ) +
+        # Treated and Control Unit Labels
+        {
+            if (include_labs) {
+                geom_text(
+                    data = labs,
+                    mapping = aes(x = x, y = y, label = label), size = 5, family = "merriweather"
+                )
+            }
+        } +
+        # Mean Labels
+        annotate("text",
+            x = dist_t - 0.025, y = mean_t + 0.15, label = "bar(Delta*y[T])",
+            parse = T, hjust = 1, size = 6, color = "#6A6599", family = "merriweather"
+        ) +
+        annotate("text",
+            x = dist_c - 0.025, y = mean_c + 0.15, label = "bar(Delta*y[C])",
+            parse = T, hjust = 1, size = 6, color = "#79AF97", family = "merriweather"
+        ) +
+        # Difference-in-differences Labels
+        annotate("point", x = dist_t, y = c(mean_t, mean_c), size = 2) +
+        annotate("segment",
+            x = dist_t, xend = dist_t, y = mean_t, yend = mean_c,
+            linetype = "dashed", size = 1.1
+        ) +
+        annotate("text",
+            x = dist_t + 0.03, y = mean_c + (mean_t - mean_c) * 2 / 3,
+            label = paste0("hat(tau) == ", round(mean_t - mean_c, 2)),
+            parse = T, hjust = 0, size = 6
+        ) +
+        # Color of lines
+        scale_color_manual(
+            values = c("Counterfactual Trend" = "grey60", "Treatment Effect" = "grey20")
+        ) +
+        # Fill of shaded regions
+        scale_fill_manual(
+            values = c("Treated Ring" = "#6A6599", "Control Ring" = "#79AF97")
+        ) +
+        # Make x-axis start at 0
+        scale_x_continuous(expand = expansion(mult = c(0, 0)), limits = c(0, 1.5)) +
+        # Labels
+        labs(
+            title = title,
+            x = "Distance from Treatment (miles)",
+            y = "Change in Outcome",
+            color = NULL, fill = NULL,
+        ) +
+        # Themeing
+        theme_kyle(base_size = 15, title_pos = "center", has_subtitle = T) +
+        theme(
+            legend.position = "bottom",
+            plot.title = element_text(margin = margin(b = 12, unit = "pt"), size = 14),
+            axis.title.y = element_text(margin = margin(r = 4), hjust = 0),
+            axis.title.x = element_text(margin = margin(t = 4))
+        ))
 }
 
 
-# ---- (a) Correct -------------------------------------------------------------
+## (a) Correct
 
-(plot_a <- plot_est(df1, 1, 1.5))
+(plot_a <- plot_est(df, df_long, 0.75, 1.5, "(a) Correct Specification", include_labs = T))
 
-# ---- (b) Treated too wide ----------------------------------------------------
+## (b) Treated too wide
 
-(plot_b <- plot_est(df1, 1.5, 2))
+(plot_b <- plot_est(df, df_long, 1, 1.5, "(b) Treated is Too Wide", include_labs = F))
 
-# ---- (c) Treated too small ----------------------------------------------------
+## (c) Treated too small
 
-(plot_c <- plot_est(df1, 0.5, 1.5))
+(plot_c <- plot_est(df, df_long, 0.5, 1.5, "(c) Treated is Too Narrow", include_labs = F))
 
-# ---- Export ------------------------------------------------------------------
+## (d) Robustness check equals (c)
 
-ggsave("figures/example_a.jpeg", plot_a, dpi = 300, width = 10, height = 5, bg="white")
-ggsave("figures/example_b.jpeg", plot_b, dpi = 300, width = 10, height = 5, bg="white")
-ggsave("figures/example_c.jpeg", plot_c, dpi = 300, width = 10, height = 5, bg="white")
+(plot_d <- plot_est(df, df_long, 0.25, 1, "(d) Robustness Check equals (c)", include_labs = F))
+
+
+# Patchwork combine
+
+(plot <- (plot_a + plot_b) / (plot_c + plot_d) + plot_layout(guides = "collect") &
+    theme(
+        legend.position = "bottom"
+    ) &
+    guides(color = guide_legend(override.aes = list(size = 3)))
+)
+
+# Export
+
+ggpreview(plot, width = 13, height = 8, device = "pdf", cairo = FALSE)
+ggsave("figures/example.pdf", plot, dpi = 300, width = 13, height = 8, bg = "white")
+
+
+
+
+
+
+
 
 
 
